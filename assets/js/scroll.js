961b6fbe7a632637a7d6d1da0c85abd5cb4f48a7
sunnyfur <polin.makarova@mail.ru>
2021-12-14 10:36:34 +0500

    change scroll anim

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,10 +1,9 @@
 window.onload = () => {
 
-    // var square = document.querySelector('.card-text__item');
     const options = {
         root: null,
         rootMargin: '0px',
-        threshold: 0.3
+        threshold: 0.9
     }
     const observer = new IntersectionObserver((entries, observer) => {
         entries.forEach(entry => {
@@ -21,47 +20,7 @@ window.onload = () => {
     const animUl = document.querySelectorAll('.card-text__item');
 
     animUl.forEach(elem => {
-        // for (let index = 0; index < animUl.length; index++) {
+
         observer.observe(elem);
     })
 }
-
-
-
-
-// var animUl = document.querySelectorAll('.card-text__item');
-// if (animUl.length > 0) {
-//     function animOnScroll(params) {
-//         for (let index = 0; index < animUl.length; index++) {
-//             const animItem = animOnScroll[index];
-//             const animItemHeight = animItem.offsetHeight;
-//             const animItemOffset = offset(animItem).top;
-//             const animStart = 4;
-
-//             let animItemPoint = window.innerHeight - animItemHeight / animStart;
-//             if (animItemHeight > window.innerHeight) {
-//                 animItemPoint = window.innerHeight - window.innerHeight / animStart;
-//             }
-
-//             if ((window.pageYOffset > animItemOffset - animItemPoint) && window.pageYOffset < (animItemOffset + animItemHeight)) {
-//                 animItem.classList.add('card-text__item-animation');
-//             }
-//             // } else {
-//             //     animItem.classList.remove('card-text__item-animation');
-//             // }
-
-
-
-//         }
-//     }
-
-//     function offset(el) {
-//         const rect = el.getBoundingClientRect(),
-//             scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
-//             scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-//         return {
-//             top: rect.top + scrollTop,
-//             left: rect.left + scrollLeft
-//         }
-//     }
-// }
